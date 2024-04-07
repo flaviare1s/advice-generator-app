75cffc5eb9a0ebe5efeed0fc3ad188d1fc3a8f28
Flávia Reis <flaviarster@gmail.com>
2024-04-07 18:11:15 -0300

    I added font family and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Manrope': ['Manrope', 'sans-serif'],
+      },
+      colors: {
+        light_cyan: 'hsl(193, 38%, 86%)',
+        neon_green: 'hsl(150, 100%, 66%)',
+        grayish_blue: 'hsl(217, 19%, 38%)',
+        dark_grayish_blue: 'hsl(217, 19%, 24%)',
+        dark_blue: 'hsl(218, 23%, 16%)',
+      },
+    },
   },
   plugins: [],
 }
-
